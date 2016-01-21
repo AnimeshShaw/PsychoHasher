1223727c4ca6e285e2d05e571a73385aa13152f0
AnimeshShaw <psychocoder@outlook.com>
2016-01-21 11:03:00 +0530

    added another method

--- src/net/letshackit/psychohasher/HashingUtils.java
+++ src/net/letshackit/psychohasher/HashingUtils.java
@@ -12,18 +12,18 @@ import java.security.NoSuchAlgorithmException;
 
 /**
  * Core class containing Hashing Utility functions.
- * 
+ *
  * @author <b>Psycho_Coder </b> (<em>Animesh Shaw</em>)
  */
 public class HashingUtils {
 
     /**
      * Converts array of bytes to hex string.
-     * 
+     *
      * @param bytes Byte Array to be converted to Hex String.
      * @return Returns the hex string for {@code bytes} array.
      */
-    private String byteArrayToHex(byte[] bytes) {
+    private static String byteArrayToHex(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < bytes.length; i++) {
             sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).
@@ -33,13 +33,14 @@ public class HashingUtils {
     }
 
     /**
-     * <p>Hash of string data following a particular algorithm.</p>
-     * 
+     * <p>
+     * Hash of string data following a particular algorithm.</p>
+     *
      * @param data String data whose hash will be computed.
      * @param hashAlgo Algorithm to be used to hash {@code data}
      * @return Returns the hash of string data as Hex String.
      */
-    private String getHash(String data, String hashAlgo) {
+    public static String getHash(String data, String hashAlgo) {
         byte[] mdBytes = null;
         try {
             MessageDigest md = MessageDigest.getInstance(hashAlgo);
@@ -53,22 +54,46 @@ public class HashingUtils {
     }
 
     /**
-     * <p>Returns the hash of the file whose path and the type of Hashing Algo
+     * <p>
+     * Hash of string data following a particular algorithm.</p>
+     *
+     * @param data String data whose hash will be computed.
+     * @param hashAlgo Hashing algorithm to be used. It is of type
+     * {@link net.letshackit.psychohasher.HashType}
+     * @see net.letshackit.psychohasher.HashType
+     * @return Hex encoded hash of the string data.
+     */
+    public static String getHash(String data, HashType hashAlgo) {
+        byte[] mdBytes = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance(hashAlgo.getValue());
+            md.update(data.getBytes());
+            mdBytes = md.digest();
+        } catch (NoSuchAlgorithmException ex) {
+            System.err.println("Couldn't determine the hashing algorithm." + ex.
+                    getMessage());
+        }
+        return byteArrayToHex(mdBytes);
+    }
+
+    /**
+     * <p>
+     * Returns the hash of the file whose path and the type of Hashing Algo
      * scheme is passed as arguments to the method.</p>
      *
-     * @param path path of the file whose hash is to be calculated. {@code path} 
+     * @param path path of the file whose hash is to be calculated. {@code path}
      * is of type {@link java.nio.file.Path}
      * @param hashAlgo Hashing algorithm to be used.
      * @return Hex encoded hash of the file
      */
-    private String getFileHash(Path path, String hashAlgo) {
+    public static String getFileHash(Path path, String hashAlgo) {
         byte[] mdbytes = null;
 
         try {
             if (Files.notExists(path, LinkOption.NOFOLLOW_LINKS)) {
                 throw new FileNotFoundException("File not found!");
             }
-            
+
             if (Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)) {
                 throw new IllegalArgumentException("Path shouldn't be a directory");
             }
@@ -92,48 +117,51 @@ public class HashingUtils {
         }
 
         return byteArrayToHex(mdbytes);
-    } 
-    
+    }
+
     /**
-     * <p>Returns the hash of the file whose path and the type of Hashing Algo
+     * <p>
+     * Returns the hash of the file whose path and the type of Hashing Algo
      * scheme is passed as arguments to the method.</p>
      *
-     * @param path path of the file whose hash is to be calculated. {@code path} 
+     * @param path path of the file whose hash is to be calculated. {@code path}
      * is of type {@link java.nio.file.Path}
-     * @param hashAlgo Hashing algorithm to be used. It is of type 
+     * @param hashAlgo Hashing algorithm to be used. It is of type
      * {@link net.letshackit.psychohasher.HashType}
      * @see net.letshackit.psychohasher.HashType
      * @return Hex encoded hash of the file
      */
-    private String getFileHash(Path path, HashType hashAlgo) {
+    public static String getFileHash(Path path, HashType hashAlgo) {
         return getFileHash(path, hashAlgo.getValue());
     }
 
     /**
-     * <p>Returns the hash of the file whose path and the type of Hashing Algo
+     * <p>
+     * Returns the hash of the file whose path and the type of Hashing Algo
      * scheme is passed as arguments to the method.</p>
      *
-     * @param file location of the file whose hash is to be calculated. {@code file} 
-     * is of type {@link java.io.File}
+     * @param file location of the file whose hash is to be calculated.
+     * {@code file} is of type {@link java.io.File}
      * @param hashAlgo Hashing algorithm to be used.
      * @return Hex encoded hash of the file
      */
-    private String getFileHash(File file, String hashAlgo) {
+    public static String getFileHash(File file, String hashAlgo) {
         return getFileHash(file.toPath(), hashAlgo);
     }
 
     /**
-     * <p>Returns the hash of the file whose path and the type of Hashing Algo
+     * <p>
+     * Returns the hash of the file whose path and the type of Hashing Algo
      * scheme is passed as arguments to the method.</p>
      *
-     * @param file location of the file whose hash is to be calculated. {@code file} 
-     * is of type {@link java.io.File}
-     * @param hashAlgo Hashing algorithm to be used. It is of type 
+     * @param file location of the file whose hash is to be calculated.
+     * {@code file} is of type {@link java.io.File}
+     * @param hashAlgo Hashing algorithm to be used. It is of type
      * {@link net.letshackit.psychohasher.HashType}
      * @see net.letshackit.psychohasher.HashType
      * @return Hex encoded hash of the file
      */
-    private String getFileHash(File file, HashType hashAlgo) {
+    public static String getFileHash(File file, HashType hashAlgo) {
         return getFileHash(file.toPath(), hashAlgo.getValue());
     }
 }
